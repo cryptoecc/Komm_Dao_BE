512329f95df48e0573b230091301bdca7f7c67d6
LiberVance <libervance@LiberVanceui-MacBookPro-2.local>
2024-10-18 12:02:08 +0900

    update

--- src/routes/controllers/user/profile/profile.controller.js
+++ src/routes/controllers/user/profile/profile.controller.js
@@ -3,6 +3,7 @@ const {
   UserPointsHistory,
   UserDealInterest,
   DealInfo,
+  USER_RATING,
 } = require("../../../../../models");
 const { sequelize } = require("../../../../../models");
 const path = require("path");
@@ -25,6 +26,7 @@ const contractABI = require(path.join(
   __dirname,
   "../../../../config/contract-abi/XpClaim"
 ));
+
 const getProfile = async (req, res) => {
   const { walletAddress } = req.params;
 
@@ -45,6 +47,13 @@ const getProfile = async (req, res) => {
       raw: true,
     });
 
+    // 사용자가 클레임한 프로젝트 ID의 개수 조회
+    const claimedProjectsCount = await UserPointsHistory.count({
+      where: { wallet_addr: walletAddress },
+      distinct: true,
+      col: "project_id", // distinct한 project_id의 개수를 구함
+    });
+
     // 프로필 데이터 생성
     const profile = {
       name: user.user_name || "Unknown User",
@@ -57,6 +66,7 @@ const getProfile = async (req, res) => {
       profileImage: user.user_image_link || "default-profile.png",
       xp: user.cur_xp || 0, // 프론트에서 'xp'로 사용하도록 데이터를 변환
       pointHistory: pointHistory || [],
+      claimedProjectsCount: claimedProjectsCount || 0, // 클레임된 프로젝트 개수 추가
     };
 
     // 사용자 정보와 포인트 히스토리를 함께 반환
@@ -121,12 +131,19 @@ const updateProfile = async (req, res) => {
 };
 
 // XP 트랜잭션 보내는 기능
+const truncateProjectName = (name, maxLength) => {
+  return name.length > maxLength ? name.substring(0, maxLength) + "..." : name;
+};
+
+// 프론트에서 서명받고 서버단에서 admin transaction 코드
 const updateXP = async (req, res) => {
-  const { walletAddress, xpPoints, projectId } = req.body;
-  console.log(projectId);
+  const { walletAddress, xpPoints, projectId, signature, projectName } =
+    req.body;
+
+  // 프로젝트 이름 잘라내기
+  const truncatedProjectName = truncateProjectName(projectName, 12);
 
   try {
-    // 먼저 해당 프로젝트에 대해 이미 XP 클레임이 이루어졌는지 확인
     const existingClaim = await UserPointsHistory.findOne({
       where: {
         wallet_addr: walletAddress,
@@ -140,15 +157,12 @@ const updateXP = async (req, res) => {
         .json({ message: "XP has already been claimed for this project." });
     }
 
-    // 스마트 계약과 상호작용 (claimXP는 트랜잭션 발생)
     const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-    // 가스 추정
     const gasEstimate = await contract.methods
       .claimXP(walletAddress, xpPoints)
       .estimateGas({ from: adminAddress });
 
-    // 트랜잭션 객체 생성
     const tx = {
       from: adminAddress,
       to: contractAddress,
@@ -158,7 +172,6 @@ const updateXP = async (req, res) => {
       maxFeePerGas: web3.utils.toWei("50", "gwei"),
     };
 
-    // 트랜잭션 서명 및 전송 (클레임한 XP에 대한 트랜잭션)
     const signedTx = await web3.eth.accounts.signTransaction(
       tx,
       adminPrivateKey
@@ -171,7 +184,6 @@ const updateXP = async (req, res) => {
       return res.status(500).json({ message: "Transaction failed" });
     }
 
-    // getXP 호출로 사용자의 총 XP 잔액을 가져옴
     const xpBalanceRaw = await contract.methods.getXP(walletAddress).call();
     const xpBalance = parseInt(xpBalanceRaw, 10);
 
@@ -179,38 +191,140 @@ const updateXP = async (req, res) => {
       return res.status(500).json({ message: "Failed to fetch XP balance" });
     }
 
-    // 현재 XP 조회 후 클레임한 XP를 더해 업데이트
     const user = await UserInfo.findOne({
       where: { wallet_addr: walletAddress },
     });
-    const currentXP = user ? user.cur_xp : 0; // 현재 XP가 없다면 0으로 설정
+    const currentXP = user ? user.cur_xp : 0;
     const updatedXP = currentXP + xpPoints;
 
-    // UserInfo 테이블에 사용자의 총 XP 잔액을 업데이트
     await UserInfo.update(
-      { cur_xp: updatedXP }, // 클레임한 XP만 더해 총 XP 업데이트
+      { cur_xp: updatedXP },
       { where: { wallet_addr: walletAddress } }
     );
 
-    // 포인트 히스토리 테이블에 이번 클레임한 xpPoints 값 기록 (project_id 추가)
+    // // UserPointsHistory에 잘라낸 프로젝트 이름 추가
+    // await UserPointsHistory.create({
+    //   wallet_addr: walletAddress,
+    //   date: moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
+    //   participation: "Discover",
+    //   activity: `Voted on Project ${truncatedProjectName}`, // 잘라낸 프로젝트 이름 사용
+    //   xp_earned: xpPoints,
+    //   transaction_id: receipt.transactionHash,
+    //   project_id: projectId,
+    // });
+
+    // UserPointsHistory에 프로젝트 이름 그대로 추가
     await UserPointsHistory.create({
       wallet_addr: walletAddress,
       date: moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
-      participation: "Project Rating",
-      activity: "Claim XP",
-      xp_earned: xpPoints, // 이번에 클레임한 XP만 기록
+      participation: "Discover",
+      activity: `Voted on Project ${projectName}`, // 잘라낸 프로젝트 이름 사용
+      xp_earned: xpPoints,
       transaction_id: receipt.transactionHash,
-      project_id: projectId, // project_id를 기록하여 중복 클레임 방지
+      project_id: projectId,
     });
 
     res.json({ success: true, transactionHash: receipt.transactionHash });
   } catch (error) {
-    console.error("Error processing XP claim:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error("Error processing XP claim:", error); // 구체적인 오류 출력
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
+// const updateXP = async (req, res) => {
+//   const { walletAddress, xpPoints, projectId } = req.body;
+//   console.log(projectId);
+
+//   try {
+//     // 먼저 해당 프로젝트에 대해 이미 XP 클레임이 이루어졌는지 확인
+//     const existingClaim = await UserPointsHistory.findOne({
+//       where: {
+//         wallet_addr: walletAddress,
+//         project_id: projectId,
+//       },
+//     });
+
+//     if (existingClaim) {
+//       return res
+//         .status(400)
+//         .json({ message: "XP has already been claimed for this project." });
+//     }
+
+//     // 스마트 계약과 상호작용 (claimXP는 트랜잭션 발생)
+//     const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+//     // 가스 추정
+//     const gasEstimate = await contract.methods
+//       .claimXP(walletAddress, xpPoints)
+//       .estimateGas({ from: adminAddress });
+
+//     // 트랜잭션 객체 생성
+//     const tx = {
+//       from: adminAddress,
+//       to: contractAddress,
+//       data: contract.methods.claimXP(walletAddress, xpPoints).encodeABI(),
+//       gas: gasEstimate,
+//       maxPriorityFeePerGas: web3.utils.toWei("2", "gwei"),
+//       maxFeePerGas: web3.utils.toWei("50", "gwei"),
+//     };
+
+//     // 트랜잭션 서명 및 전송 (클레임한 XP에 대한 트랜잭션)
+//     const signedTx = await web3.eth.accounts.signTransaction(
+//       tx,
+//       adminPrivateKey
+//     );
+//     const receipt = await web3.eth.sendSignedTransaction(
+//       signedTx.rawTransaction
+//     );
+
+//     if (!receipt.transactionHash) {
+//       return res.status(500).json({ message: "Transaction failed" });
+//     }
+
+//     // getXP 호출로 사용자의 총 XP 잔액을 가져옴
+//     const xpBalanceRaw = await contract.methods.getXP(walletAddress).call();
+//     const xpBalance = parseInt(xpBalanceRaw, 10);
+
+//     if (isNaN(xpBalance)) {
+//       return res.status(500).json({ message: "Failed to fetch XP balance" });
+//     }
+
+//     // 현재 XP 조회 후 클레임한 XP를 더해 업데이트
+//     const user = await UserInfo.findOne({
+//       where: { wallet_addr: walletAddress },
+//     });
+//     const currentXP = user ? user.cur_xp : 0; // 현재 XP가 없다면 0으로 설정
+//     const updatedXP = currentXP + xpPoints;
+
+//     // UserInfo 테이블에 사용자의 총 XP 잔액을 업데이트
+//     await UserInfo.update(
+//       { cur_xp: updatedXP }, // 클레임한 XP만 더해 총 XP 업데이트
+//       { where: { wallet_addr: walletAddress } }
+//     );
+
+//     // 포인트 히스토리 테이블에 이번 클레임한 xpPoints 값 기록 (project_id 추가)
+//     await UserPointsHistory.create({
+//       wallet_addr: walletAddress,
+//       date: moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
+//       participation: "Project Rating",
+//       activity: "Claim XP",
+//       xp_earned: xpPoints, // 이번에 클레임한 XP만 기록
+//       transaction_id: receipt.transactionHash,
+//       project_id: projectId, // project_id를 기록하여 중복 클레임 방지
+//     });
+
+//     res.json({ success: true, transactionHash: receipt.transactionHash });
+//   } catch (error) {
+//     console.error("Error processing XP claim:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
 // XP 잔액을 업데이트하는 API
+// 프론트에서 트랜잭션 보낼 경우 아래의 API 로 XP balance 만 업데이트
+
 const updateXPBalance = async (req, res) => {
   const { walletAddress, xpBalance } = req.body;
 
@@ -414,10 +528,10 @@ module.exports = {
   getProfile,
   updateProfile,
   updateXP,
+  updateXPBalance,
   fetchAndUpdateXPBalance,
   getUserPointHistory,
   updatePointHistory,
-  updateXPBalance,
   getUserDealInterest,
   checkAlreadyClaimed,
 };
